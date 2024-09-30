c15679b8048c5a8f937237588c3b92160d29c554
lushij <766993892@qq.com>
2024-09-30 13:06:43 +0800

    链表

---
+++ 线性表实战/链表实战/main.cpp
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+typedef int ElemType;
+typedef struct LNode {
+	ElemType data;
+	struct LNode* next;
+}Lnode,*LinkList;
+void insert_head_list(LinkList& l)
+{
+	ElemType data = 0;
+	scanf("%d", &data);	
+	while (data != 9999)
+	{
+		LinkList tmp = (LinkList)malloc(sizeof(Lnode));
+		tmp->data = data;
+		tmp->next = l->next;
+		l->next = tmp;
+		scanf("%d", &data);
+	}
+}
+void insert_tail_list(LinkList& l)
+{
+	LinkList cur = l;
+	ElemType data = 0;
+	scanf("%d", &data);
+	while (data != 9999)
+	{
+		LinkList tmp = (LinkList)malloc(sizeof(Lnode));
+		tmp->next = cur->next;
+		tmp->data = data;
+		cur->next = tmp;
+		cur = tmp;
+		scanf("%d", &data);
+	}
+}
+void print_list(LinkList& l)
+{
+	LinkList cur = l->next;
+	while (cur)
+	{
+		printf("%d ", cur->data);
+		cur = cur->next;
+	}
+	printf("\n");
+}
+int main()
+{
+	LinkList l = (LinkList)malloc(sizeof(Lnode));//存储头节点的头指针
+	l->next = NULL;
+	//insert_head_list(l);
+	insert_tail_list(l);
+	print_list(l);
+	return 0;
+}
