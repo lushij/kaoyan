c80c43f6a7a8d3e59e24e9f1eefddd90b6fdb9af
lushij <766993892@qq.com>
2024-10-28 12:41:52 +0800

    二叉排序树删除查找

--- 二叉树专题/二叉排序树/main.cpp
+++ 二叉树专题/二叉排序树/main.cpp
@@ -14,6 +14,8 @@ typedef struct tree_node {
 //特点，左子树都小于根节点，小于右子树
 //考中序遍历,查找
 
+
+//核心函数：构造二叉排序树，此版本为递归版本
 int bst_insert(BTree& root, int val)
 {
     //每次新来一个先建一个新节点
@@ -46,6 +48,7 @@ void create_bst_tree(BTree &root,int *nums,int len)
         bst_insert(root, nums[i]);//核心函数
     }
 }
+//中序遍历
 void mid_print(BTree t)
 {
     if (t)
@@ -55,6 +58,80 @@ void mid_print(BTree t)
         mid_print(t->rc);
     }
 }
+
+//查找
+void find_tree(BTree tree,ElemType key)
+{
+    BTree cur = tree;
+    while (cur)
+    {
+        if (key > cur->val)
+        {
+            //右子树
+            cur = cur->rc;
+        }
+        else if (key < cur->val)
+        {
+            cur = cur->lc;
+        }
+        else
+        {
+            printf("在二叉排序树中找到了%d\n",key);
+            break;
+        }
+    }
+}
+
+//删除：此版本为递归版本
+void delete_tree(BTree &tree,ElemType del)
+{
+    if (tree == NULL)
+    {
+        return;
+    }
+    if (del < tree->val)
+         delete_tree(tree->lc, del);
+    else if ( del > tree->val)
+         delete_tree(tree->rc, del);
+    else
+    {
+        //找到了要删除的节点
+        if (!tree->lc)//等价于tree->lc == NULL
+        {
+            //左子树为空，右子树直接代替顶上去就行
+            BTree tmp = tree;
+            tree = tree->rc;
+            free(tmp);
+        }
+        else if (tree->rc == NULL)
+        {
+            //右子树为空，左子树直接代替顶上去就行
+            BTree tmp = tree;
+            tree = tree->lc;
+            free(tmp);
+        }
+        else
+        {
+            //左右子树都不为空，是最为困难的一种
+            //策略1：找到左子树的最右边即最大的节点
+            //策略2：找到右子树的最左边即最小的节点
+            //本次使用策略1
+            BTree cur = tree->lc;//查找左子树分支的最大节点
+            while (cur->rc!=NULL)
+            {
+                cur = cur->rc;
+            }
+            //更换待删除节点和cur此时的值
+            tree->val = cur->val;
+            delete_tree(tree->lc, cur->val);//删除此时被更换的节点,在左子树分支上
+        }
+            
+        
+    }
+}
+
+
+
 
 void numslen(int nums[])
 {
@@ -70,6 +147,13 @@ int main()
     numslen(nums);*/
     create_bst_tree(trees, nums,len);
     mid_print(trees);
+    puts("");
+    //find_tree(trees, 65);
+    //find_tree(trees, 15);
+    //find_tree(trees, 100);
+    delete_tree(trees, 25);
+    mid_print(trees);
+    puts("");
     return 0;
 }
 
